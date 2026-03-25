52707b04dc7df9b43d1bbdfbab44c2c2433cc860
Eduardo <eduao154@gmail.com>
2026-03-24 23:56:17 -0400

    Update NodeRouters.js

--- Backend/NodeRouters.js
+++ Backend/NodeRouters.js
@@ -23,12 +23,16 @@ const server = http.createServer(app);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// Middlewares
+
 app.use(
   cors({
-    origin: ["https://slack-d.netlify.app/","http://localhost:3000", "http://localhost:5173","https://socialmedia-khe0.onrender.com" ], 
+    origin: [
+      "https://gorgeous-lebkuchen-0e9856.netlify.app",
+      "http://localhost:3000",
+      "http://localhost:5173"
+    ],
     credentials: true,
-  }),
+  })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
