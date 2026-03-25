c4964fb8d7d82b7346e228946c89124b1cbf13ba
Eduardo <eduao154@gmail.com>
2026-03-24 23:04:05 -0400

    Update db.js

--- Backend/prisma/db.js
+++ Backend/prisma/db.js
@@ -1,3 +1,4 @@
+/*
 // src/lib/prisma.js
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
@@ -11,6 +12,27 @@ const pool = new Pool({
 })
 
 const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
+
+export default prisma
+ */
+
+import { PrismaPg } from '@prisma/adapter-pg'
+import pkg from '@prisma/client'
+import dotenv from 'dotenv'
+import { Pool } from 'pg'
+
+dotenv.config()
+
+const { PrismaClient } = pkg
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ssl: { rejectUnauthorized: false } // descomenta si tu hosting lo requiere
+})
+
+const adapter = new PrismaPg(pool)
+
 const prisma = new PrismaClient({ adapter })
 
 export default prisma
