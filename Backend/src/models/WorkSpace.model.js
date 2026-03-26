3dc0e005e5adfa92c94221b7fe26009bbd2e5611
Eduardo <eduao154@gmail.com>
2026-03-25 22:41:17 -0400

    Update WorkSpace.model.js

--- Backend/src/models/WorkSpace.model.js
+++ Backend/src/models/WorkSpace.model.js
@@ -29,20 +29,9 @@ router.get("/workspace_members", TokenVerifyAuth, async (req, res) => {
   }
 });
 
-
-router.get("/details",   async (req, res) => {
-  try {
-    
-
-    
-
-    const workspace_members_Data = await db.workspace_members.findMany( );
-
-    return res.status(200).json({ ok: true, data: workspace_members_Data });
-  } catch (error) {
-    console.error("workspace_members error:", error);
-    res.status(500).json({ ok: false, message: "Internal server error" });
-  }
+router.get("/details", async (req, res) => {
+  console.log("HIT /private/details");
+  return res.json({ ok: true, message: "details route working" });
 });
 
 export default router;
