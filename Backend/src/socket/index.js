31de71efbdb49312e53875c2958356b332341885
Eduardo <eduao154@gmail.com>
2026-04-04 18:11:10 -0400

    Update index.js

--- Backend/src/socket/index.js
+++ Backend/src/socket/index.js
@@ -1,5 +1,5 @@
+import directMessagesSocket from "../../src/socket/directMessagesInfo.js";
 import channelMessagesSocket from "./channelMessages.socket.js";
-import directMessagesSocket from "./directMessagesInfo.socket.js";
 import reactionsSocket from "./reactions.socket.js";
 
 export default function registerSockets(io, socket, userId) {
