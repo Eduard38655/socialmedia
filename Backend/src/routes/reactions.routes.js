f956e8ad2d42a83704a3efede0ad87b40b1b2a7a
Eduardo <eduao154@gmail.com>
2026-04-03 22:48:48 -0400

    Update reactions.routes.js

--- Backend/src/routes/reactions.routes.js
+++ Backend/src/routes/reactions.routes.js
@@ -1,6 +1,6 @@
 // reactions.routes.js
 import express from "express";
-import { getReactions } from "../controller/Reactions.controller.js";
+import getReactions from "../controller/Reactions.controller.js";
 import TokenVerifyAuth from "../middleware/TokenVerify.auth.js";
 
 const router = express.Router();
